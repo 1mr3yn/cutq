76344698ff5db9ea08e22fe2ce7d1cae3ad109ef
ryanbayona <1mr3yn@gmail.com>
2016-11-12 19:05:31 +0800

    twilio api test call

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
-
+use Twilio\Rest\Client;
 /*
 |--------------------------------------------------------------------------
 | Application Routes
@@ -12,5 +12,38 @@
 */
 
 $app->get('/', function () use ($app) {
-    return $app->version();
+  return $app->version();
+});
+
+$app->get("test-twilio",function(){
+   
+    //require_once "vendor/autoload.php";
+    
+    
+    // Step 2: Set our AccountSid and AuthToken from https://twilio.com/console
+    $AccountSid = env("TWILIO_SID");
+    $AuthToken = env("TWILIO_AUTH_TOKEN");
+
+    // Step 3: Instantiate a new Twilio Rest Client
+    $client = new Client($AccountSid, $AuthToken);
+
+    try {
+        // Initiate a new outbound call
+        $call = $client->account->calls->create(
+            // Step 4: Change the 'To' number below to whatever number you'd like 
+            // to call.
+            "+639990505595",
+
+            // Step 5: Change the 'From' number below to be a valid Twilio number 
+            // that you've purchased or verified with Twilio.
+            "+16467986317",
+
+            // Step 6: Set the URL Twilio will request when the call is answered.
+            array("url" => "http://demo.twilio.com/welcome/voice/")
+        );
+        echo "Started call: " . $call->sid;
+    } catch (Exception $e) {
+        echo "Error: " . $e->getMessage();
+    }
+
 });
